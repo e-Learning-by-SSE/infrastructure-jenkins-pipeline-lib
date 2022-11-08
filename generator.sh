8d44e288683bd5d1617ab03bcfaf7f4ff61ed294
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2022-11-08 07:08:43 +0100

    Revised package script improves availability chk

--- generator.sh
+++ generator.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-TITLE='\033[0;31m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+TITLE='\033[1;34m'
 NORMAL='\033[0m'
 
+chk_availability() {
+    attempt_counter=0
+    max_attempts=10
+    SERVER="$1"
+    until $(curl --output /dev/null --silent --head --fail "$SERVER"); do
+        if [ ${attempt_counter} -eq ${max_attempts} ];then
+        echo -e "${RED}Max attempts reached${NORMAL}"
+        return 1
+        fi
+
+        printf '.'
+        attempt_counter=$(($attempt_counter+1))
+        sleep 5
+    done
+    echo -e "${GREEN}Server reachable at: ${NORMAL}$SERVER"
+
+    # Wait additional time to avoid empty responses
+    sleep 10
+    return 0
+}
 
 echo -e "${TITLE}Comptence-Repository${NORMAL}"
 API_URL="https://staging.sse.uni-hildesheim.de:9010/api-json"
@@ -13,9 +35,11 @@ PACKAGE="${ARTIFACT_ID}"
 MODEL_PACKAGE="model"
 API_PACKAGE="api"
 
-if ./chk_server_online.sh $API_URL; then
+if chk_availability $API_URL ; then
+    rm -f "${ARTIFACT_ID}*.zip"
+
+    # Python
     mvn clean compile -Dspec_source=$API_URL -Dversion=$VERSION -Dlanguage=$LANG -Dgroup_id=$GROUP_ID -Dartifact_id=$ARTIFACT_ID -Dpackage=$PACKAGE -Dmodel=$MODEL_PACKAGE -Dapi=$API_PACKAGE
-    rm -f "${ARTIFACT_ID}_${LANG}.zip"
     cd target/generated-sources/swagger/ > /dev/null
     zip -r -q ../../../"${ARTIFACT_ID}_${LANG}.zip" .
     cd - > /dev/null
