930a4408760a1d22eb817e8b3d30fb38ebeaa151
spark-sse <spark@uni-hildesheim.de>
2023-03-03 14:58:04 +0100

    fix:keep target dir with openapi.json

--- vars/generateSwaggerClient.groovy
+++ vars/generateSwaggerClient.groovy
@@ -5,7 +5,7 @@ def call(apiPath, version, groupId, artifactId, languages) {
   configFileProvider([configFile(fileId: 'maven-openapi-generator', variable: 'MAVEN_POM')]) {
     languages.each { lang ->
       sh "cp -v $MAVEN_POM ./generator-pom.xml" // avoid target/ dir in the tmp jenkins dir
-      sh "mvn -f generator-pom.xml clean compile -Dbasedir=. -Dspec_source=${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${packageName} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
+      sh "mvn -f generator-pom.xml compile -Dbasedir=. -Dspec_source=${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${packageName} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
       sh "zip -r -q ../../../${artifactId}_${lang}.zip target/generated-sources/swagger/"   
       sh "rm ./generator-pom.xml"
    }
