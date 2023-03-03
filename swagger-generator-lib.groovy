1f9239f5ef9dae1481177c4200345b46055b80f5
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-03-03 12:06:19 +0100

    Update swagger-generator-lib.groovy

--- swagger-generator-lib.groovy
+++ swagger-generator-lib.groovy
@@ -1,8 +1,10 @@
-def generateSwaggerClient(apiPath, version, groupId, artifactId, modelPackage, apiPackage, ...languages)
-  def PACKAGE="${ARTIFACT_ID}"
+def generateSwaggerClient(apiPath, version, groupId, artifactId, ...languages)
+  def package=artifactId
+  def modelPackage=PACKAGE + ".model"
+  def apiPackage=PACKAGE + ".api"
   configFileProvider([configFile(fileId: 'maven-openapi-generator', variable: 'MAVEN_POM')]) {
-    strings.each { languages ->
-      sh "mvn -f $MAVEN_POM clean compile -Dspec_source= ${API_PATH} -Dversion=${VERSION} -Dlanguage=${LANG} -Dgroup_id=${GROUP_ID} -Dartifact_id=${ARTIFACT_ID} -Dpackage=${PACKAGE} -Dmodel=${MODEL_PACKAGE} -Dapi=${API_PACKAGE}"
+    strings.each { lang ->
+      sh "mvn -f $MAVEN_POM clean compile -Dspec_source= ${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${package} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
     }
   }
   sh "zip -r -q ../../../${ARTIFACT_ID}_${LANG}.zip target/generated_sources/swagger/"
