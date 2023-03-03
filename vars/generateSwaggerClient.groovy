17b547fb26f6b522d167f133c442c8d3e53cbfda
spark-sse <spark@uni-hildesheim.de>
2023-03-03 23:56:20 +0100

    fix: don't use dots in non-java languages

--- vars/generateSwaggerClient.groovy
+++ vars/generateSwaggerClient.groovy
@@ -1,15 +1,17 @@
 def call(apiPath, version, groupId, artifactId, languages) {
-  def packageName = artifactId
-  def modelPackage = packageName + ".model"
-  def apiPackage = packageName + ".api"
   configFileProvider([configFile(fileId: 'maven-openapi-generator', variable: 'MAVEN_POM')]) {
     sh "cp -v $MAVEN_POM ./generator-pom.xml" // avoid target/ dir in the tmp jenkins dir
     languages.each { lang ->
       if (lang == 'java') {
         mavenPhase = 'compile install deploy'
         packageName = "${groupId}.${artifactId}"
+        modelPackage = packageName + ".model"
+        apiPackage = packageName + ".api"
       } else {
         mavenPhase = 'compile'
+        packageName = artifactId
+        modelPackage = 'model'
+        apiPackage = 'api'
       }
       sh "mvn -f generator-pom.xml ${mavenPhase} -Dspec_source=${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${packageName} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
       sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/swagger/"
