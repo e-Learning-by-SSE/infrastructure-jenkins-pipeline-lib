e2944fb6f6b4f5c8e12cb3ce1fecd3f31a7db241
spark-sse <spark@uni-hildesheim.de>
2023-03-03 15:46:23 +0100

    archive artifacts after build

--- vars/generateSwaggerClient.groovy
+++ vars/generateSwaggerClient.groovy
@@ -3,11 +3,12 @@ def call(apiPath, version, groupId, artifactId, languages) {
   def modelPackage = packageName + ".model"
   def apiPackage = packageName + ".api"
   configFileProvider([configFile(fileId: 'maven-openapi-generator', variable: 'MAVEN_POM')]) {
+    sh "cp -v $MAVEN_POM ./generator-pom.xml" // avoid target/ dir in the tmp jenkins dir
     languages.each { lang ->
-      sh "cp -v $MAVEN_POM ./generator-pom.xml" // avoid target/ dir in the tmp jenkins dir
       sh "mvn -f generator-pom.xml compile -Dbasedir=. -Dspec_source=${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${packageName} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
-      sh "zip -r -q ../../../${artifactId}_${lang}.zip target/generated-sources/swagger/"   
-      sh "rm ./generator-pom.xml"
-   }
+      sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/swagger/"
+      archiveArtifacts artifacts: "${artifactId}_${lang}.zip"
+    }
+    sh "rm ./generator-pom.xml" 
   }
 }
