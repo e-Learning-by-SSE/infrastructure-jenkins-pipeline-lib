322c3d23b3d4cb902087b1d14e9377f36fe9844b
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-31 13:49:39 +0200

    Test to use only one POM version

--- vars/generateSwaggerClient.groovy
+++ vars/generateSwaggerClient.groovy
@@ -1,15 +1,8 @@
 def call(apiPath, version, groupId, artifactId, languages) {
   configFileProvider([configFile(fileId: 'maven-openapi-generator', variable: 'MAVEN_POM')]) {
 	
+	sh "cp -v $MAVEN_POM ./generator-pom.xml"
     languages.each { lang ->
-	  // Avoid target/ dir in the tmp jenkins dir
-	  if (lang.startsWith('typescript')) {
-		// New version, may be incompatible, not tested with all APIs
-		sh "cp -v "pom_openapi.xml" ./generator-pom.xml"
-      } else {
-		// Deprecated consider update
-		sh "cp -v $MAVEN_POM ./generator-pom.xml"
-	  }
 	
       if (lang == 'java') {
         mavenPhase = 'compile install deploy'
@@ -24,15 +17,9 @@ def call(apiPath, version, groupId, artifactId, languages) {
       }
       sh "mvn -f generator-pom.xml ${mavenPhase} -Dspec_source=${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${packageName} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
       
-	  // Zip correct folder based on POM file that was used
-	  if (lang.startsWith('typescript')) {
-		sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/openapi/"
-      } else {
-		sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/swagger/"
-	  }
-
+	  sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/openapi/"
       archiveArtifacts artifacts: "${artifactId}_${lang}.zip"
-	  sh "rm ./generator-pom.xml" 
     }
+	sh "rm ./generator-pom.xml" 
   }
 }
