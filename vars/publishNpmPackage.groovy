5c4c4e7bf8d8a4df4e70ffba9f4f0f12d4f684cd
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-31 13:58:29 +0200

    Adds PWD for debugging

--- vars/publishNpmPackage.groovy
+++ vars/publishNpmPackage.groovy
@@ -1,6 +1,7 @@
 def call(path, token, registry='https://npm.pkg.github.com/') {
   sh '''
     cd ${path}
+	pwd
 	npm i
 	npm set ${registry}:_authToken ${token}
 	npm publish --access public --registry=${registry}
