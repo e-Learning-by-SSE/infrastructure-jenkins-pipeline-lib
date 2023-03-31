3e10e70974b12e0455e9025df29d5845b9a8402e
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-31 14:32:32 +0200

    Alternative handling of registry param

--- vars/publishNpmPackage.groovy
+++ vars/publishNpmPackage.groovy
@@ -1,7 +1,8 @@
 def call(path, token, registry='https://npm.pkg.github.com/') {
+  echo "$registry"
   dir("$path") {
 	sh 'npm i'
-	sh 'npm set ${registry}:_authToken ${token}'
-	sh 'npm publish --access public --registry=${registry}'
+	sh 'npm set "$registry":_authToken ${token}'
+	sh 'npm publish --access public --registry="$registry"'
   }
 }
