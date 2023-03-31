fef7d71bc178e458ed9a0aea90fcf670fc08d197
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-31 15:14:14 +0200

    Cleanup

--- vars/publishNpmPackage.groovy
+++ vars/publishNpmPackage.groovy
@@ -1,9 +1,9 @@
 def call(path, token, registry='//npm.pkg.github.com/') {
-  echo "$registry"
   dir("$path") {
 	sh 'npm i'
 	sh 'rm -f ~/.npmrc'
 	sh "echo $registry:_authToken=$token >> ~/.npmrc"
 	sh "npm publish --access public --registry=https:${registry}"
+	sh 'rm -f ~/.npmrc'
   }
 }
