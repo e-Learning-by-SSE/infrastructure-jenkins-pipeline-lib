f4658becb5f9b86816d0c9903e0dd9f4deef5ad3
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-31 14:56:26 +0200

    Alternative handling of access token

--- vars/publishNpmPackage.groovy
+++ vars/publishNpmPackage.groovy
@@ -1,10 +1,10 @@
-def call(path, token, registry='https://npm.pkg.github.com/') {
+def call(path, token, registry='//npm.pkg.github.com/') {
   echo "$registry"
   dir("$path") {
 	sh 'npm i'
-	sh """
-	  npm set ${registry}:_authToken ${token}
-	  npm publish --access public --registry=${registry}
-	"""
+	sh 'rm -f ~/.npmrc'
+	sh 'echo $registry:_authToken=$NPM_PUBLSH_TOKEN >> ~/.npmrc'
+	sh 'npm publish --access public --registry=https://$registry'
+	sh 'rm -f ~/.npmrc'
   }
 }
