8391e87c10f7ac53da25775213d1faccaf40f85f
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-03-31 13:33:25 +0200

    Fixes Zip of archives

--- vars/generateSwaggerClient.groovy
+++ vars/generateSwaggerClient.groovy
@@ -23,9 +23,16 @@ def call(apiPath, version, groupId, artifactId, languages) {
         apiPackage = 'api'
       }
       sh "mvn -f generator-pom.xml ${mavenPhase} -Dspec_source=${apiPath} -Dversion=${version} -Dlanguage=${lang} -Dgroup_id=${groupId} -Dartifact_id=${artifactId} -Dpackage=${packageName} -Dmodel=${modelPackage} -Dapi=${apiPackage}"
-      sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/swagger/"
+      
+	  // Zip correct folder based on POM file that was used
+	  if (lang.startsWith('typescript')) {
+		sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/openapi/"
+      } else {
+		sh "zip -r -q ${artifactId}_${lang}.zip target/generated-sources/swagger/"
+	  }
+
       archiveArtifacts artifacts: "${artifactId}_${lang}.zip"
+	  sh "rm ./generator-pom.xml" 
     }
-    sh "rm ./generator-pom.xml" 
   }
 }
