1b5b868175d940bbd7556b0273ac4860d1569f84
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-04-24 12:17:20 +0200

    NPM Package Check: Renamed parameter
    Avoid naming problems with protected keywords

--- vars/checkNpmPackageExist.groovy
+++ vars/checkNpmPackageExist.groovy
@@ -1,9 +1,9 @@
-def call(package, version, token, registry='//npm.pkg.github.com/') {
+def call(npmName, version, token, registry='//npm.pkg.github.com/') {
     sh 'rm -f ~/.npmrc'
     sh "echo $registry:_authToken=$token >> ~/.npmrc"
 	
     script {
-	    def versionList = sh(returnStdout: true, script: "npm view $package versions --json"
+	    def versionList = sh(returnStdout: true, script: "npm view $npmName versions --json"
 		sh 'rm -f ~/.npmrc'
 		
 		if (versionList.contains($version)) {
