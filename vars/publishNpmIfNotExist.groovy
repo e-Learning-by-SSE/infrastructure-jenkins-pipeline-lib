35484f662aacffff6a76731969207918e07bbe22
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-04-24 13:24:59 +0200

    Default parameter fix

--- vars/publishNpmIfNotExist.groovy
+++ vars/publishNpmIfNotExist.groovy
@@ -1,4 +1,4 @@
-def call(groupName, pkgName, version, path, readOnlyToken, publishToken, registry='//npm.pkg.github.com/') {
+def call(groupName, pkgName, version, path, readOnlyToken, publishToken, registry="//npm.pkg.github.com/") {
     withCredentials([
 		string(credentialsId: "$publishToken", variable: 'Auth'),
 		string(credentialsId: "$readOnlyToken", variable: 'ReadOnly')
