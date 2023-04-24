401876643f1cfb98bdf7f2da3ac84bc1b3e7c1d8
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-04-24 14:23:35 +0200

    Code formating

--- vars/checkNpmPackageExist.groovy
+++ vars/checkNpmPackageExist.groovy
@@ -4,16 +4,16 @@ def call(groupName, pkgName, version, token, registry='//npm.pkg.github.com/') {
 		sh "echo $registry:_authToken=$token >> ~/.npmrc"
 	
     script {
-		def npmName = "$groupName/$pkgName"
-	    // Do not abort if package does not exist
-		def versionList = sh(returnStdout: true, script: "npm view $npmName versions --json 2>&1 && echo || echo")
-		sh 'rm -f ~/.npmrc'
-		
-		if (versionList.contains("$version")) {
-			return true
-		} else {
-		    sh "echo $version not found"
-			return false
-		}
+			def npmName = "$groupName/$pkgName"
+				// Do not abort if package does not exist
+			def versionList = sh(returnStdout: true, script: "npm view $npmName versions --json 2>&1 && echo || echo")
+			sh 'rm -f ~/.npmrc'
+			
+			if (versionList.contains("$version")) {
+				return true
+			} else {
+					sh "echo $version not found"
+				return false
+			}
     }
 }
