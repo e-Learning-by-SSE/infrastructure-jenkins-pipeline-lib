88dffe8d2f14aeb89c445724bc0d2887b115eda7
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-04-24 12:46:48 +0200

    CI: Fixed version-parameter handling

--- vars/checkNpmPackageExist.groovy
+++ vars/checkNpmPackageExist.groovy
@@ -9,7 +9,7 @@ def call(groupName, pkgName, version, token, registry='//npm.pkg.github.com/') {
 		def versionList = sh(returnStdout: true, script: "npm view $npmName versions --json 2>&1 && echo || echo")
 		sh 'rm -f ~/.npmrc'
 		
-		if (versionList.contains($version)) {
+		if (versionList.contains("$version")) {
 			return true
 		} else {
 			return false
