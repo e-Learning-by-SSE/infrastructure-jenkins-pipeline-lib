d0b9762b8d15f4bf8bcd97300d5be2268874140f
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-04-25 13:52:37 +0200

    refactor: make the method simpler

--- vars/checkNpmPackageExist.groovy
+++ vars/checkNpmPackageExist.groovy
@@ -1,19 +1,10 @@
 def call(groupName, pkgName, version, token, registry='//npm.pkg.github.com/') {
+    sh "echo $groupName:registry=https:$registry > ~/.npmrc"
+    sh "echo $registry:_authToken=$token >> ~/.npmrc"
+
+    npmName = "$groupName/$pkgName"
+    // Do not abort if package does not exist
+    versionList = sh(returnStdout: true, script: "npm view $npmName versions --json 2>&1 && echo || echo")
     sh 'rm -f ~/.npmrc'
-	  sh "echo $groupName:registry=https:$registry >> ~/.npmrc"
-		sh "echo $registry:_authToken=$token >> ~/.npmrc"
-	
-    script {
-			def npmName = "$groupName/$pkgName"
-				// Do not abort if package does not exist
-			def versionList = sh(returnStdout: true, script: "npm view $npmName versions --json 2>&1 && echo || echo")
-			sh 'rm -f ~/.npmrc'
-			
-			if (versionList.contains("$version")) {
-				return true
-			} else {
-					sh "echo $version not found"
-				return false
-			}
-    }
+    return versionList.contains("$version") ?: (sh "echo $version not found"; false)
 }
