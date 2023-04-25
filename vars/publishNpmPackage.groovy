b0c5274f945cd2d4fdbd51e8ac255c6ea221e92e
spark-sse <spark@uni-hildesheim.de>
2023-04-25 14:41:14 +0200

    fix: add missing steps block

--- vars/publishNpmPackage.groovy
+++ vars/publishNpmPackage.groovy
@@ -10,12 +10,14 @@ def call(path, token, registry='//npm.pkg.github.com/') {
         }
         stages {
             stage('NPM Publish') {
-                dir("$path") {
-                    sh 'npm i'
-                    sh 'rm -f ~/.npmrc'
-                    sh "echo $registry:_authToken=$token >> ~/.npmrc"
-                    sh "npm publish --access public --registry=https:${registry}"
-                    sh 'rm -f ~/.npmrc'
+                steps {
+                    dir("$path") {
+                        sh 'npm i'
+                        sh 'rm -f ~/.npmrc'
+                        sh "echo $registry:_authToken=$token >> ~/.npmrc"
+                        sh "npm publish --access public --registry=https:${registry}"
+                        sh 'rm -f ~/.npmrc'
+                    }
                 }
             }
         }
