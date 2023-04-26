911ddac710031f9f3eb26bea590587bdc02fef5a
spark-sse <spark@uni-hildesheim.de>
2023-04-26 11:25:09 +0200

    fix: syntax error

--- vars/postgresSidecar.groovy
+++ vars/postgresSidecar.groovy
@@ -1,4 +1,4 @@
-def call(image, postgresUser, postgresPassword, postgresDb, postgresPort,, Closure closure) {
+def call(image, postgresUser, postgresPassword, postgresDb, postgresPort, Closure closure) {
   def dbImage = 'postgres:14.3-alpine'
   
   sidecar: {
