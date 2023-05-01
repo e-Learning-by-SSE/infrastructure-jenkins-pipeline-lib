a46f2baaf13c3e0dab8e12d757c0b577cca4a841
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-01 15:25:18 +0200

    rf(npm-publish): remove insecure string interpolation

--- vars/publishNpmIfNotExist.groovy
+++ vars/publishNpmIfNotExist.groovy
@@ -1,10 +1,10 @@
 def call(groupName, pkgName, version, path, readOnlyToken, publishToken, registry='//npm.pkg.github.com/') {   
     withCredentials([
-		string(credentialsId: "$publishToken", variable: 'Auth'),
-		string(credentialsId: "$readOnlyToken", variable: 'ReadOnly')
+		string(credentialsId: publishToken, variable: 'Auth'),
+		string(credentialsId: readOnlyToken, variable: 'ReadOnly')
 	]) {
-		if (!checkNpmPackageExist("$groupName", "$pkgName", "$version", "$ReadOnly", "$registry")) {
-			publishNpmPackage("$path", "$Auth", "$registry")
+		if (!checkNpmPackageExist(groupName, pkgName, version, ReadOnly, registry)) {
+			publishNpmPackage(path, Auth, registry)
 		}
 	}
 }
