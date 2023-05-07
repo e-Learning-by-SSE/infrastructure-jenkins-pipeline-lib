b4932b604ef82a2d99995e998720774157314b36
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-07 16:13:35 +0200

    test

--- vars/packageJson.groovy
+++ vars/packageJson.groovy
@@ -1,14 +1,14 @@
 def isNewVersion() {
     def publisher = LastChanges.getLastChangesPublisher "PREVIOUS_REVISION", "SIDE", "LINE", true, true, "", "", "", "", ""
-    publisher.publishLastChanges()
+    //publisher.publishLastChanges()
     def changes = publisher.getLastChanges()
 
     for (commit in changes.getCommits()) {
         for (change in commit.getChanges()) {
             println(change)
-            if (change.getPath().endsWith("package.json") && change.getDiff().contains("version")) {
+            //if (change.getPath().endsWith("package.json") && change.getDiff().contains("version")) {
                 return true
-            }
+            //}
         }
     }
     return false
