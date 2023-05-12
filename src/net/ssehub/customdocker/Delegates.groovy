c7bfe8d1979d271d22cbe11fd1b7f58ed77003cf
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-12 17:24:33 +0200

    try to use the correct build method

--- src/net/ssehub/customdocker/Delegates.groovy
+++ src/net/ssehub/customdocker/Delegates.groovy
@@ -3,7 +3,7 @@ package net.ssehub.customdocker
 class Delegates {
     static def call(obj, Closure cl) {
         def code = cl.rehydrate(obj, this, this)
-        code.resolveStrategy = Closure.DELEGATE_ONLY
+        code.resolveStrategy = Closure.OWNER_FIRST
         code()
     }
 }
