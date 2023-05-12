c99997322b98a7311617687a9e34879687540c80
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-12 16:47:37 +0200

    tests(docker): add dummy dockerfile

--- vars/dockerGithubPublish.groovy
+++ vars/dockerGithubPublish.groovy
@@ -1,11 +1,12 @@
-def call(Map config = [:]) { 
-  image = docker.image("${config.target}")
-  docker.withRegistry('https://ghcr.io', 'github-ssejenkins') {
-    if (config.additionalTags != null) {
+def call(image, additionalTags = [:]) {
+    if (!image.exists()) {
+      error("Image ${target} must be build in order to publish it")
+    }
+
+    docker.withRegistry('https://ghcr.io', 'github-ssejenkins') {
+      image.push() // target contains tag - push this too
       config.additionalTags.each{ tag -> 
         image.push("${tag}")
       }
     }
-    image.push() // tagged version from name
-  }
 }
