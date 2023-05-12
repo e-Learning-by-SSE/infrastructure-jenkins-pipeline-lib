9e5c05e4b18f17cc65ae94ee97469b96047dd111
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-12 17:25:07 +0200

    next try

--- src/net/ssehub/customdocker/Delegates.groovy
+++ src/net/ssehub/customdocker/Delegates.groovy
@@ -3,7 +3,7 @@ package net.ssehub.customdocker
 class Delegates {
     static def call(obj, Closure cl) {
         def code = cl.rehydrate(obj, this, this)
-        code.resolveStrategy = Closure.OWNER_FIRST
+        code.resolveStrategy = Closure.OWNER_ONLY
         code()
     }
 }
