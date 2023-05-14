2adb49a70dcaff1766670628e2f29645aa8e9d3b
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-14 10:58:49 +0200

    try to fix unresolvable methods

--- src/net/ssehub/customdocker/Delegates.groovy
+++ src/net/ssehub/customdocker/Delegates.groovy
@@ -3,7 +3,7 @@ package net.ssehub.customdocker
 class Delegates {
     static def call(obj, Closure cl) {
         def code = cl.rehydrate(obj, this, this)
-        code.resolveStrategy = Closure.OWNER_ONLY
+        code.resolveStrategy = Closure.DELEGATES_FIRST
         code()
     }
 }
