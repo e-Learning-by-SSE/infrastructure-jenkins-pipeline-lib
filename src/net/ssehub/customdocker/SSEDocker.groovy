b1afdc923437e5b055097649a1f74844dae73fa9
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-14 23:05:25 +0200

    fix(docker): remove non existing image check

--- src/net/ssehub/customdocker/SSEDocker.groovy
+++ src/net/ssehub/customdocker/SSEDocker.groovy
@@ -49,9 +49,6 @@ class SSEDocker {
             if (image == null) {
                 image = docker.image(imageName)
             }
-            if (!image.exists()) {
-                error("Image ${target} must be build in order to publish it")
-            }
             docker.withRegistry('https://ghcr.io', 'github-ssejenkins') {
                     image.push() // target contains tag - push this too
                     config.additionalTags.each{ tag -> 
