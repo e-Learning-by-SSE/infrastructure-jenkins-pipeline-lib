1d7b4c8866c7ad4a257581245e401e162e6f616b
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-14 10:59:13 +0200

    fix spelling error

--- src/net/ssehub/customdocker/Delegates.groovy
+++ src/net/ssehub/customdocker/Delegates.groovy
@@ -3,7 +3,7 @@ package net.ssehub.customdocker
 class Delegates {
     static def call(obj, Closure cl) {
         def code = cl.rehydrate(obj, this, this)
-        code.resolveStrategy = Closure.DELEGATES_FIRST
+        code.resolveStrategy = Closure.DELEGATE_FIRST
         code()
     }
 }
