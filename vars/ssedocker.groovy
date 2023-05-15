8020483f233fe3f3003d2415a02155e45a8b3898
spark-sse <113825305+spark-sse@users.noreply.github.com>
2023-05-15 15:20:09 +0200

    testing

--- vars/ssedocker.groovy
+++ vars/ssedocker.groovy
@@ -2,7 +2,9 @@ import net.ssehub.customdocker.SSEDocker
 import net.ssehub.customdocker.Delegates
 
 def call(Closure closure) {
-    def ssedocker = new SSEDocker(docker)
-    Delegates.call(ssedocker, closure)
-    ssedocker.execute()
+    node {
+        def ssedocker = new SSEDocker(docker)
+        Delegates.call(ssedocker, closure)
+        ssedocker.execute()
+    }
 }
